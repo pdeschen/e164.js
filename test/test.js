879d010b83f534bcb11d5362dd43849edf167a74
Pascal Deschenes <pascal.deschenes@nuecho.com>
2014-07-01 16:49:05 -0400

    more test cases

--- test/test.js
+++ test/test.js
@@ -11,6 +11,8 @@ assert.equal("India", e164.lookup('9191'));
 assert.equal("Norfolk Island", e164.lookup('672322424'));
 assert.equal("ICO Global (Mobile Satellite Service)", e164.lookup('88112311'));
 assert.equal("Canada", e164.lookup('1418'));
+assert.equal("United States", e164.lookup('1603'));
+assert.equal("Malaysia", e164.lookup('603'));
 assert.equal(undefined, e164.lookup('0'));
 
 console.log("ok");
